b554c9009c7d4eca736e78b8572ad1ea885900ec
Kyle Chine <kyle.chine@leismore.co>
2021-03-14 09:02:59 +1100

    src done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export { PlainText, LB, Text, canoLB, xmlLB, SP2 } from './lib/PlainText';
+export {
+    PlainText,
+    LB     as PlainText_LB,
+    Text   as PlainText_Text,
+    canoLB as PlainText_canoLB,
+    xmlLB  as PlainText_xmlLB,
+    SP2    as PlainText_SP2
+} from './lib/PlainText';
